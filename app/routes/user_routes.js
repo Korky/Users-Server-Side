9b167516d65d2229441cec25bfbd8ba9c4e3d3c0
Fernando J Rojas Sola <korkstr@gmail.com>
2018-02-07 12:22:50 -0500

    First Commit

---
+++ app/routes/user_routes.js
@@ -0,0 +1,77 @@
+var ObjectID = require('mongodb').ObjectID;
+
+module.exports = function(app, db) {
+    
+    app.get('/users',(req,res) => {
+        db.collection('users').find({}).toArray(function(err, item){
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                res.send(item);
+            }
+        });
+    });
+    
+    app.get('/users/:id', (req, res) => {
+        const id = req.params.id
+        const details = { '_id': new ObjectID(id) };
+        db.collection('users').findOne(details, (err, item) => {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                res.send(item);
+            }
+        });
+    });
+    
+ 
+    app.delete('/users/:id', (req, res) => {
+        const id = req.params.id
+        const details = { '_id': new ObjectID(id) };
+        db.collection('users').remove(details, (err, item) => {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                res.send('User '+id+' deleted');
+            }
+        });
+    });
+    
+    app.put('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const user = { 
+            user_name:req.body.user_name,
+            phone: req.body.phone,
+            email: req.body.email,
+            auth_roles: req.body.auth_roles,
+            last_actions: req.body.last_actions,
+            last_login: req.body.last_login
+        }
+    db.collection('users').update(details, user, (err, result) => {
+      if (err) {
+          res.send({'error':'An error has occurred'});
+      } else {
+          res.send(user);
+      } 
+    });
+  });
+    
+    app.post('/users', (req, res) => {
+        const user = { 
+            user_name:req.body.user_name,
+            phone: req.body.phone,
+            email: req.body.email,
+            auth_roles: req.body.auth_roles,
+            last_actions: req.body.last_actions,
+            last_login: req.body.last_login
+        }
+        db.collection('users').insert(user,(err, result)=>{
+           if(err){
+               res.send({'error' : 'An error has occurred' });
+           } else{
+               res.send(result.ops[0]);
+           }
+        });
+    });
+};
---
+++ config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+    
+    url : 'mongodb://admin-2808:admin1234@ds125618.mlab.com:25618/users-db'
+}
